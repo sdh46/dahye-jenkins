dc91dff90822f9a4ce380d742804eca226bb84a9
dahye <sdh46@chosun.kr>
2023-01-02 21:22:55 +0900

    Initial commit

---
+++ src/main/java/com/example/dahyejenkins/DahyeJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dahyejenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DahyeJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DahyeJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/dahyejenkins/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.dahyejenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "Hello";
+    }
+}
+
+///1f436f3586c1219dbfa243d50ace13ef685d2bc5
+
+//    mvn sonar:sonar \
+//        -Dsonar.projectKey=sefsfe \
+//        -Dsonar.host.url=http://133.186.151.141:9000 \
+//        -Dsonar.login=1f436f3586c1219dbfa243d50ace13ef685d2bc5
